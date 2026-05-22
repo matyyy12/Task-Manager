80f496431ce6550231a146080021f26db374cb89
mateusz <matikugu@gmail.com>
2026-05-22 18:17:28 +0200

    repaired forntend

--- backend/task/views.py
+++ backend/task/views.py
@@ -23,6 +23,7 @@ class TaskListView(APIView):
         if user_pk is not None:
             filters['user'] = user_pk
 
+
         if group_pk is not None:
             filters['group'] = group_pk
         else:
